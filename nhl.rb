2a2f6d9a325f43ee9e7ebf3c7453ef0dd3f4e821
Ivan Mironov <ivan-mironov@binomo.com>
2016-04-27 20:36:15 +0300

    added initial parser

---
+++ nhl.rb
@@ -0,0 +1,35 @@
+require 'rubygems'
+require 'nokogiri'
+require 'open-uri'
+
+page = Nokogiri::HTML(open("http://www.nhl.com/scores/htmlreports/20152016/PL020001.HTM"))
+
+events = page.css("tr[class='evenColor']").map do |event|
+  props = event.css("td")
+  time = props[3].text
+  pos = time.index(':')
+  prop_hash =
+    {
+      id:      props[0].text.to_i,
+      period:  props[1].text.to_i,
+      str:     props[2].text,
+      time:    time[0..pos + 2],
+      elapsed: time[pos + 3..-1],
+      event:   props[4].text.downcase.to_sym,
+      desc:    props[5].text,
+    }
+  if prop_hash[:event] != :gend
+    players = props[6].css('font').map do |player|
+      title = player['title'].split(" - ")
+      {
+        position: title[0].downcase.gsub(' ', '_').to_sym,
+        name:     title[1],
+        number:   player.text.to_i,
+      }
+    end
+    prop_hash.merge!(players: players)
+  end
+  prop_hash
+end
+
+puts events.select { |event| event[:event] == :goal}
